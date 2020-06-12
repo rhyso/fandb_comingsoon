2ce202b2118c7b8f0d122ad6e01f50c7fb5c7fa1
rhyso <rhyswork@gmail.com>
2020-06-12 19:33:25 +0100

    made responsive

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${props => props.primary ? "palevioletred" : "white"};
+  color: ${props => props.primary ? "white" : "palevioletred"};
+
+  font-size: 1em;
+  margin-top: 1em;
+  margin-bottom: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  padding:10px 20px;
+`;
+
 
 function App() {
   return (
     <div className="App">
      <div className="main-div">
-      <h1>The new way to hire event space in the countryside</h1>
+      <h1>The new way to hire event space in the outdoors</h1>
       <h2>Sign up to hear updates</h2>
+     </div>
+
+     <div className="email-input">
+       <input type="text" placeholder="Email Address" />
+       <Button primary>Sign up</Button>
      </div>
     </div>
   );
