303a22269c2d8d186251aca1e630432dab3493db
rhyso <rhyswork@gmail.com>
2020-06-12 18:47:47 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="main-div">
+      <h1>The new way to hire event space in the countryside</h1>
+      <h2>Sign up to hear updates</h2>
+     </div>
     </div>
   );
 }
