bc50085bd60d0ec0cec45609223c9c0f2210d54b
rhyso <rhyswork@gmail.com>
2020-06-13 22:43:01 +0100

    setup for express to serve for heroku

---
+++ client/build/precache-manifest.4f014de9f90493a03fac529812d46dee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf1143b206dccd3fa69ce37bc9e8e1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db3eea07d1da934a70d8",
+    "url": "/static/css/main.092c1b4f.chunk.css"
+  },
+  {
+    "revision": "c5c45ba54e9fb2ab62c3",
+    "url": "/static/js/2.5b7c7220.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5b7c7220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db3eea07d1da934a70d8",
+    "url": "/static/js/main.5f0fc345.chunk.js"
+  },
+  {
+    "revision": "e9b4d0559780176ec5f7",
+    "url": "/static/js/runtime-main.0e81e8ef.js"
+  },
+  {
+    "revision": "3b57e4e4f32c3cb645ac345c25dea21d",
+    "url": "/static/media/amyfield.3b57e4e4.jpg"
+  }
+]);
